876b54a145bcda69bfb463adaeb6ad06f7f8c16f
Sabina Gušić <sabinagusic3@gmail.com>
2026-01-22 00:14:38 +0100

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-center text-white mt-30">
+      <div>
+        <h1 className="text-3xl font-bold mt-30">Sabina Portofolio</h1>
+      </div>
+      <div>
+        <h1 className="text-3xl font-bold">Sabina Portofolio</h1>
+      </div>
+      <div>
+        <h1 className="text-3xl font-bold mt-30">Sabina Portofolio</h1>
+      </div>
+      <div>
+        <h1 className="text-3xl font-bold">Sabina Portofolio</h1>
+      </div>
+      <div>
+        <h1 className="text-3xl font-bold mt-1000">Sabina Portofolio</h1>
+      </div>
+    </main>
+  );
+}
